77a61c5e6eac396ebd1593b88a6ea907ed421d2a
aokiken <aokiken@me.com>
2019-10-03 19:34:05 +0900

    第2章 明白な実装 終了時点のコード

--- __tests__/MoneyTest.ts
+++ __tests__/MoneyTest.ts
@@ -2,6 +2,8 @@ import { Dollar } from "../src/Dollar"
 
 test('Multiplication', () => {
     const five = new Dollar(5)
-    five.times(2)
-    expect(10).toEqual(five.amount)
+    let product = five.times(2)
+    expect(10).toEqual(product.amount)
+    product = five.times(3)
+    expect(15).toEqual(product.amount)
 })
