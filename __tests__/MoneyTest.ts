468134c0846098237aad791bb1401bc93b4c9fa3
aokiken <aokiken@me.com>
2019-10-03 20:38:25 +0900

    第11章 不要になったら消す 終了時点のコード

--- __tests__/MoneyTest.ts
+++ __tests__/MoneyTest.ts
@@ -9,15 +9,7 @@ test('Multiplication', () => {
 test('Equality', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true)
     expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false)
-    expect(Money.franc(5).equals(Money.franc(5))).toBe(true)
-    expect(Money.franc(5).equals(Money.franc(6))).toBe(false)
     expect(Money.franc(5).equals(Money.dollar(5))).toBe(false)
-})
-
-test('FrancMultiplication', () => {
-    const five = Money.franc(5)
-    expect(Money.franc(10)).toEqual(five.times(2))
-    expect(Money.franc(15)).toEqual(five.times(3))
 })
 
 test('Currency', () => {
@@ -25,6 +17,3 @@ test('Currency', () => {
     expect('CHF').toEqual(Money.franc(1).currency)
 })
 
-test('DifferentClassEquality', () => {
-    expect(new Money(10, 'CHF').equals(new Franc(10, 'CHF'))).toBe(true)
-})
